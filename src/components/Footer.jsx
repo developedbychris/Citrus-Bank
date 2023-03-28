78fc87580e1311a97ea9fd9637356fe512d016c6
Chris Maya <killchrisu@gmail.com>
2023-03-28 14:08:17 -0400

    added animation for socials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -47,7 +47,7 @@ const Footer = () => (
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">2023 CitrusBank. All Rights Reserved.</p>
       <div className="flex flex-row md:mt-0 mt-6">
         {socialMedia.map((social, idx)=>(
-          <img src={social.icon} alt={social.id} key={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${idx !== socialMedia.length -1 ? "mr-6" : "mr-0" } hover:scale-125 duration-200`}/>
+          <img src={social.icon} alt={social.id} key={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${idx !== socialMedia.length -1 ? "mr-6" : "mr-0" } hover:animate-bounce`}/>
         ))}
       </div>
     </div>
