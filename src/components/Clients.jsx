5ce428f194cac383a46526005b11e07a133a4605
Chris Maya <killchrisu@gmail.com>
2023-03-29 11:12:17 -0400

    added margin to client images

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -6,7 +6,7 @@ const Clients = () => (
   <section className={`${styles.flexCenter} my-4`}>
     <div className={`${styles.flexCenter} flex-wrap w-full`}>
       {clients.map(client =>(
-        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
           <a href={client.link} target="_blank"><img src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain filter brightness-50 hover:brightness-100 duration-200'/></a>
         </div>
       ))}
